a08231dbf4f32244e267131bb8dac1f1bf90fb3a
Yashraj Agarwal <50520646+yashraj236@users.noreply.github.com>
2024-11-06 18:26:39 +0530

    Add files via upload

---
+++ Spring1/src/main/java/org/example/Alien.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.example.Laptop;
+
+public class Alien {
+
+    private int age;
+    //private Laptop lap = new Laptop();
+    private Laptop lap = new Laptop();
+
+    public Alien() {
+        System.out.println("Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {    // Setter Injection
+        System.out.println("Setter called");
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public void code() {
+        System.out.println("Coding");
+        lap.compile();
+    }
+
+}
---
+++ Spring1/src/main/java/org/example/Laptop.java
@@ -0,0 +1,13 @@
+
+package org.example;
+
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop object created");
+    }
+
+    public void compile() {
+        System.out.println("Compiling");
+    }
+}
